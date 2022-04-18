7572b5376f72b66d2ef5e9874242484552e7261a
JaydenDev <92550746+JaydenDev@users.noreply.github.com>
2022-04-18 11:58:04 -0500

    Create Database

---
+++ data.js
@@ -0,0 +1,8 @@
+let databaseData = [
+  // add facts here, this is the data, this is the database. Remember, each fact is to be put in "" and have a , at the end
+  "JavaScript took just 10 days to develop.",
+  "JavaScript was probably named after Java.",
+  "GitHub says JavaScript is the Most Popular language in the world.",
+  "There are around 700 separate programming languages",
+  "The first computer virus was created in 1986",
+]
